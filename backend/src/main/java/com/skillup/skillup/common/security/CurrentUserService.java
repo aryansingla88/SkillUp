6ecd0a45c6383d41c2a9d120e98438aa4b8822aa
aryansingla88 <aryansingla1302@gmail.com>
2026-09-12 17:51:04 +0530

    Implemented Correct CurrentUserService

--- backend/src/main/java/com/skillup/skillup/common/security/CurrentUserService.java
+++ backend/src/main/java/com/skillup/skillup/common/security/CurrentUserService.java
@@ -1,14 +1,25 @@
 package com.skillup.skillup.common.security;
 
-import com.skillup.skillup.common.exception.ResourceNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurrentUserService {
 
     public Long getCurrentUserId() {
-        return 1L; // temporary local testing
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+
+        Number userId = jwt.getClaim("userId");
+
+        if (userId == null) {
+            throw new IllegalStateException("User ID not found in JWT");
+        }
+
+        return userId.longValue();
     }
 }
